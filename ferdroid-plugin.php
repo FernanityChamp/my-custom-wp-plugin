5e7e41a581dac02b5e98cd9954135a5b5cd6944a
Andres Fernando <andres.fernando08@gmail.com>
2020-04-10 10:56:26 -0500

    config clase inicial con hooks

--- ferdroid-plugin.php
+++ ferdroid-plugin.php
@@ -16,3 +16,46 @@ Text Domain: ferdroid-plugin
 /*
 Disclaimer of the License
 */
+
+
+// if ( ! defined('ABSPATH') ) {
+//     die;
+// }
+
+defined( 'ABSPATH' ) or die('Hey, you can\'t access this file');
+
+// if ( ! function_exists( 'add_action') ) {
+//     echo "Hey, you can't access this file";
+//     exit;
+// }
+
+
+class FerdroidPlugin 
+{
+    function activate() {
+        
+    }
+
+    function deactivate() {
+
+    }
+
+    function uninstall() {
+        
+    }
+
+}
+
+if ( class_exists('FerdroidPlugin') ) {
+    $ferdroidPlugin = new FerdroidPlugin();
+}
+
+
+// activation
+register_activation_hook( __FILE__, array($ferdroidPlugin, 'activate') );
+
+// deactivation
+register_deactivation_hook( __FILE__, [ $ferdroidPlugin, 'deactivate' ] );
+
+// uninstall
+
