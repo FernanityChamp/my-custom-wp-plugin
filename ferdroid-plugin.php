3af37b2da34d47f4995fc47da37a338cc75ac21e
Andres Fernando <andres.fernando08@gmail.com>
2020-04-09 20:42:10 -0500

    setup inicial del plugin

---
+++ ferdroid-plugin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package FerdroidPlugin
+ */
+/*
+Plugin Name: Ferdroid Plugin
+Plugin URI: http://ferdinania.com/get-plugins
+Description: This is my first attempt on writing a custom Plugin
+Version: 1.0.0
+Author: FernanityChamp
+Author URI: http://ferdinania.com
+License: GPLv2 or later
+Text Domain: ferdroid-plugin
+*/
+
+/*
+Disclaimer of the License
+*/
